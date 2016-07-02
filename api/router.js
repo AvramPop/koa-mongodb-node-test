766d53ae391661608aef7755a1991f51ca7f3ef2
AvramPop <dani_pop08@yahoo.com>
2016-07-02 19:55:46 +0300

    fixed routes
    added routes for person candidate and voter

--- api/router.js
+++ api/router.js
@@ -4,12 +4,30 @@ var route = require('koa-route');
 // Route definitions
 module.exports = function router(app)
 {
-  // Set TODO routes
-  app.use(route.get('/', require('./routes/list')));
-  app.use(route.get('/todo/new', require('./routes/add')));
-  app.use(route.get('/todo/:id', require('./routes/show')));
-  app.use(route.get('/todo/delete/:id', require('./routes/remove')));
-  app.use(route.get('/todo/edit/:id', require('./routes/edit')));
-  app.use(route.post('/todo/create', require('./routes/create')));
-  app.use(route.post('/todo/update', require('./routes/update')));
+  // Set PERSON routes
+  app.use(route.get('/', require('./routes/person/list')));
+  app.use(route.get('/person/new', require('./routes/person/add')));
+  app.use(route.get('/person/:id', require('./routes/person/show')));
+  app.use(route.get('/person/delete/:id', require('./routes/person/remove')));
+  app.use(route.get('/person/edit/:id', require('./routes/person/edit')));
+  app.use(route.post('/person/create', require('./routes/person/create')));
+  app.use(route.post('/person/update', require('./routes/person/update')));
+
+  // Set CANDIDATE routes
+  app.use(route.get('/candidate', require('./routes/candidate/list')));
+  app.use(route.get('/candidate/new', require('./routes/candidate/add')));
+  app.use(route.get('/candidate/:id', require('./routes/candidate/show')));
+  app.use(route.get('/candidate/delete/:id', require('./routes/candidate/remove')));
+  app.use(route.get('/candidate/edit/:id', require('./routes/candidate/edit')));
+  app.use(route.post('/candidate/create', require('./routes/candidate/create')));
+  app.use(route.post('/candidate/update', require('./routes/candidate/update')));
+
+  // Set VOTER routes
+  app.use(route.get('/voter', require('./routes/voter/list')));
+  app.use(route.get('/voter/new', require('./routes/voter/add')));
+  app.use(route.get('/voter/:id', require('./routes/voter/show')));
+  app.use(route.get('/voter/delete/:id', require('./routes/voter/remove')));
+  app.use(route.get('/voter/edit/:id', require('./routes/voter/edit')));
+  app.use(route.post('/voter/create', require('./routes/voter/create')));
+  app.use(route.post('/voter/update', require('./routes/voter/update')));
 };
